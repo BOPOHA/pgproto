c0710a3f384c6011c795c8f983aef3f603f79ece
Anatolii Vorona <vorona.tolik@gmail.com>
2021-03-07 22:38:51 +0100

    optimize ssl message encoding

--- ssl.go
+++ ssl.go
@@ -16,10 +16,10 @@ func ParseSSLResponse(r io.Reader) error {
 
 func (s *SSLRequest) Encode() []byte {
 	// [int32 - length] []byte \0
-	w := newWriteBuffer()
-	w.WriteInt(sslRequestVersion)
-	w.PrependLength()
-	return w.Bytes()
+	//w := newWriteBuffer()
+	//w.WriteInt(sslRequestVersion)
+	//w.PrependLength()
+	return []byte{0x0, 0x0, 0x0, 0x8, 0x4, 0xd2, 0x16, 0x2f}
 }
 
 func (s *SSLRequest) AsMap() map[string]interface{} {
